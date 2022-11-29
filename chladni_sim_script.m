acb6800fc8c491247fc28d95a4e64996791299ae
megham1nd <megha.rao@stonybrook.edu>
2022-11-28 22:21:55 -0500

    updated sim script

--- chladni_sim_script.m
+++ chladni_sim_script.m
@@ -2,6 +2,18 @@
 length = pi;
 width = pi;
 
+%define conditions of the wave function
+%% later on we can try and loop through multiple different m and n
+%% and find different patterns
+n = 1;
+m = 1;
+a, bnm = define_constants(length, width, n, m);
+
+%initial conditions u(x, y, 0) = func_init(x, y)
+%% specific initial displacements result in Chladni patterns
+%% and nodal lines
+func_init = @(x, y) sin(a*x)*sin(y);
+
 %making the MATLAB mesh
 [x, y] = meshgrid(0:0.1:pi);
 z = pass;
@@ -13,12 +25,14 @@ mesh(x, y, z)
 %displaying the resulting Chladni patterns
 
 %%LOCAL FUNCTIONS
-function [a, b] = define_constants(l, w)
+function [a, bnm] = define_constants(l, w, n, m)
+    %define constants
     a = w/l;
-    b = 1;
+    bnm = 1;
 end
 
-function f = init_conditions()
-    f = 1;
+function u = wave(t)
+    %wave as a function of time
+    u = 1;
 end
 
