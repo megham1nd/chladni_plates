bb9a12bf0755a161929b5aec1e7084ccc2dc13dc
megham1nd <megha.rao@stonybrook.edu>
2022-11-29 00:18:51 -0500

    update sim script

--- chladni_sim_script.m
+++ chladni_sim_script.m
@@ -1,40 +1,33 @@
 %define dimensions of the membrane
 length = pi;
 width = pi;
+%define a
+a = width/length;
 
-%define conditions of the wave function
-%% later on we can try and loop through multiple different m and n
-%% and find different patterns
-n = 1;
-m = 1;
+%making the MATLAB mesh
+[X, Y] = meshgrid(0:0.1:pi);
+
 %initial conditions u(x, y, 0) = func_init(x, y)
 %% must be a periodic function - maybe we can manipulate this too
 %% and see what happens?
-func_init = @(x, y) sin(a*x)*sin(y);
-%resulting constants
-a, bnm = define_constants(length, width, n, m);
+func_init = sin(a*X).*sin(Y);
 
-%making the MATLAB mesh
-[x, y] = meshgrid(0:0.1:pi);
-z = pass;
-
-mesh(x, y, z)
+%mesh(x, y, z)
 
 %making the wave function simulation
 
 %displaying the resulting Chladni patterns
 
 %%LOCAL FUNCTIONS
-function [a, bnm] = define_constants()
-    %define a
-    a = w/l;
+function bnm = define_bnm(n, m)
     %define b
-    func = @(x,y) func_init*sin(a*n*x)*sin(m*y);
+    func = func_init.*sin(a*n*X).*sin(m*Y); %MAKE THIS MESHGRID
     bnm = (4*a/(pi^2))*integral2(func, 0, (pi/n), 0, pi);
 end
 
-function u = wave(t)
+function z = wave(t)
     %wave as a function of time
-    u = 1;
+    syms n m;
+    z = symsum(symsum(define_bnm(n, m)*sin(a*n.*X)*sin(m.*Y)*cos(sqrt(a^2*n^2+m^2*t)), m, 1, inf), n, 1, inf);
 end
 
