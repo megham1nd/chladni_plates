e884d4d0fde52778f7413864bd852a65017da6ac
megham1nd <megha.rao@stonybrook.edu>
2022-12-06 21:10:31 -0500

    WORKING preliminary wave function

--- chladni_sim_script.m
+++ chladni_sim_script.m
@@ -5,39 +5,33 @@ width = pi;
 a = width/length;
 
 %making the MATLAB mesh
-[X, Y] = meshgrid(0:0.1:pi);
-
-%initial conditions u(x, y, 0) = func_init(x, y)
-%% must be a periodic function - maybe we can manipulate this too
-%% and see what happens?
-func_init = sin(a*X).*sin(Y);
+[X, Y] = meshgrid(0:0.1:3.2);
 
 %mesh(x, y, z)
 %making the wave function simulation
-figure;
-hold('on');
-video = VideoWriter('chladni_sim.avi');
-open(video);
-for t = 0:100
-    mesh(X, Y, wave(t))
-    drawnow;
-    writeVideo(video, getframe(gcf));
+for t = 0:0.1:100
+    mesh(X, Y, wave(X, Y, a, t))
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+    axis ([0 3.2 0 3.2 -1 1]);
+    pause(0.1);
 end
-hold('off');
-close(video);
 
 %displaying the resulting Chladni patterns
 
 %%LOCAL FUNCTIONS
-function bnm = define_bnm(n, m)
-    %define b
-    func = func_init.*sin(a*n*X).*sin(m*Y); 
-    bnm = (4*a/(pi^2))*integral2(func, 0, (pi/n), 0, pi);
+function bnm = define_bnm(x, y, a, n, m)
+    %define b 
+    %func_init = sin(a*x).*sin(y);
+    %func = func_init.*sin(a*n*x).*sin(m*y); 
+    bnm = (4*a/(pi^2))*integral2((sin(a*x).*sin(y).*sin(a*n*x).*sin(m*y)), 0, (pi/n), 0, pi);
 end
 
-function Z = wave(t)
+function Z = wave(x, y, a, t)
     %wave as a function of time
-    syms n m;
-    Z = symsum(symsum(define_bnm(n, m)*sin(a*n*X).*sin(m*Y)*cos(sqrt((a^2*n^2)+(m^2*t))), m, 1, inf), n, 1, inf);
+    %syms n m;
+    %Z = symsum(symsum(define_bnm(X, Y, a, n, m)*sin(a*n*X).*sin(m*Y)*cos(sqrt((a^2*n^2)+(m^2*t))), m, 1, inf), n, 1, inf);
+    Z = sin(a*x).*sin(y)*cos(sqrt(a^2+1)*t);
 end
 
